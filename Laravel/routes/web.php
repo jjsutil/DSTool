4be05539776ea72d42b3f3793788e3b5f8eb2028
Juan Sutil <jjsutil@uc.cl>
2025-04-04 19:24:53 -0300

    regrouping the monorepo in different directories (Python and Laravel), where the python (Fast) API will live, separated from the Filament app that includes the Frontend
    Signed-off-by: Juan Sutil <jjsutil@uc.cl>

--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
 
 Route::middleware(['auth'])->get('/dashboard', function () {
     return view('filament::dashboard');
